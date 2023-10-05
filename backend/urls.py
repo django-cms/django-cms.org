f2ff3dd443fb851c1231223f7909692db9b264ef
Fabian Braun <fsbraun@gmx.de>
2023-10-05 22:24:28 +0200

    Fix: Debug toolbar url before cms urls

--- backend/urls.py
+++ backend/urls.py
@@ -9,9 +9,12 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns.extend(path("__debug__/", include(debug_toolbar.urls)))
 
 urlpatterns.append(path('', include('cms.urls')))
 
 # the new django admin sidebar is bad UX in django CMS custom admin views.
-# admin.site.enable_nav_sidebar = False
+admin.site.enable_nav_sidebar = False
+
