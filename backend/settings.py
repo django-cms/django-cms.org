5486c1d3e2e3ff17db5215f69526ded9f9aaa509
maxnoelp2 <max@surfgreen.dev>
2026-02-11 12:38:15 +0100

    feat: update .gitignore and Dockerfile for static file handling; clean up settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -210,12 +210,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # read the setting value from the environment variable
 DEFAULT_STORAGE_DSN = os.environ.get("DEFAULT_STORAGE_DSN")
 
-# dsn_configured_storage_class() requires the name of the setting
-DefaultStorageClass = dsn_configured_storage_class("DEFAULT_STORAGE_DSN")
-
-# Django's DEFAULT_FILE_STORAGE requires the class name
-DEFAULT_FILE_STORAGE = "backend.settings.DefaultStorageClass"
-
 # only required for local file storage and serving, in development
 MEDIA_URL = "/media/"
 if DEFAULT_STORAGE_DSN:
