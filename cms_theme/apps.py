4a85ac3cd2adb353edf5211817f164d5adad1e63
Fabian Braun <fsbraun@gmx.de>
2026-03-02 17:06:15 +0100

    fix: Make heading plugin local for this project (#84)

--- cms_theme/apps.py
+++ cms_theme/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CmsThemeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cms_theme"
+
+    def ready(self):
+        from djangocms_frontend.contrib.utilities.cms_plugins import HeadingPlugin
+
+        HeadingPlugin.is_local = True
