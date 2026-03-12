23db133c15a569e71457ed17e55e658519344cf3
Fabian Braun <fsbraun@gmx.de>
2026-03-12 23:21:08 +0100

    feat: Add customer code block.

--- cms_theme/apps.py
+++ cms_theme/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class CmsThemeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cms_theme"
+
+    def ready(self) -> None:
+        from cms.plugin_pool import plugin_pool
+
+        code_plugin = plugin_pool.get_plugin("CodeBlockPlugin")
+        if code_plugin:
+            code_plugin.change_form_template = "code_block/admin/code_block.html"
+            
