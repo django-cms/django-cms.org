9a5719e8f866d111e8b4987abfe8b45554dd4257
Fabian Braun <fsbraun@gmx.de>
2026-03-21 19:42:16 +0100

    fix: fieldset syntax

--- cms_theme/cms_components.py
+++ cms_theme/cms_components.py
@@ -1129,7 +1129,7 @@ class Counter(CMSFrontendComponent):
                     "description",
                     "color_style",
                 )
-            })
+            }),
         )
 
     counter_type = forms.ChoiceField(
