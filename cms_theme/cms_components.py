d272dd7b57e334930a50fbc7b1fbbb4a95a73bcc
Fabian Braun <fsbraun@gmx.de>
2026-05-14 06:13:03 +0200

    fix: Sourcery complaint

--- cms_theme/cms_components.py
+++ cms_theme/cms_components.py
@@ -244,7 +244,7 @@ class MegaMenu(CMSFrontendComponent):
     class Meta:
         name = _("Mega Menu")
         render_template = "megamenu/menu.html"
-        allowed_models = ["djangocms_alias.aliascontent"]
+        allowed_models = ["djangocms_alias.AliasContent"]
         slots = (
             Slot("left", _("Left column"), child_classes=["TextPlugin", "TextLinkPlugin"]),
             Slot("links", _("Links (middle)")),
@@ -266,7 +266,7 @@ class Footer(CMSFrontendComponent):
     class Meta:
         name = _("Footer")
         render_template = "footer/footer.html"
-        allowed_models = ["djangocms_alias.aliascontent"]
+        allowed_models = ["djangocms_alias.AliasContent"]
         allow_children = True
         mixins = ["Background", "Spacing", "Attributes"]
         frontend_editable_fields = ("left_label", "middle_label", "right_label")
