5362fb822ecb27fb0721224f1462a5dd0f53f4d5
Fabian Braun <fsbraun@gmx.de>
2026-05-14 06:11:29 +0200

    Add allowed list for root plugins

--- cms_theme/apps.py
+++ cms_theme/apps.py
@@ -5,6 +5,8 @@ class CmsThemeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cms_theme"
 
+    root_plugins = ("TextPlugin", "MDTextPlugin", "Alias")
+
     def ready(self) -> None:
         from cms.plugin_pool import plugin_pool
         from djangocms_frontend.contrib.grid.cms_plugins import GridColumnPlugin
@@ -13,5 +15,5 @@ class CmsThemeConfig(AppConfig):
 
         plugin_pool.discover_plugins()
         for plugin_type, plugin in plugin_pool.plugins.items():
-            if plugin_type != "TextPlugin" and str(plugin.module) != "Sections" and plugin.allowed_models is None:
+            if plugin_type not in self.root_plugins and str(plugin.module) != "Sections" and plugin.allowed_models is None:
                 plugin.require_parent = True
