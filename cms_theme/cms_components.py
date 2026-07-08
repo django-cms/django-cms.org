955eacfeb557b61926444db2d11a7b65c6e18b1c
Fabian Braun <fsbraun@gmx.de>
2026-07-08 17:33:51 +0200

    fix: Cards spacing

--- cms_theme/cms_components.py
+++ cms_theme/cms_components.py
@@ -720,6 +720,7 @@ class BenefitsCards(CMSFrontendComponent):
             ("2", _("2")),
             ("3", _("3")),
             ("4", _("4")),
+            ("5", _("5")),
         ],
         initial="4",
         help_text=_("Number of grid columns."),
